13a370be6685a769016095fa46d6e78242adbc1e
Aryan Thakur <52215513+importly@users.noreply.github.com>
2025-03-14 21:51:53 -0400

    Add new configuration fields for event, match level, auto start position, and scoring metrics in reefscape_config.js

--- 2025/reefscape_config.js
+++ 2025/reefscape_config.js
@@ -12,6 +12,23 @@ var config_data = `
       "maxSize": 5,
       "required": "true"
     },
+    { "name": "Event",
+      "code": "e",
+      "type": "event",
+      "defaultValue": "2025ilpe",
+      "required": "true"
+    },
+    { "name": "Match Level",
+      "code": "l",
+      "type": "level",
+      "choices": {
+        "qm": "Quals<br>",
+        "sf": "Semifinals<br>",
+        "f": "Finals"
+      },
+      "defaultValue": "qm",
+      "required": "true"
+    },
     { "name": "Match #",
       "code": "m",
       "type": "match",
@@ -37,8 +54,16 @@ var config_data = `
       "type": "team",
       "min": 1,
       "max": 99999
-}
-
+    },
+    { "name": "Auto Start Position",
+      "code": "as",
+      "type": "clickable_image",
+      "filename": "2025/half_field.png",
+      "clickRestriction": "one",
+      "dimensions": "6 6",
+      "allowableResponses": "1 7 13 19 25 31",
+      "shape": "circle 5 black red true"
+    }
   ],
   "auton": [
     { "name": "Leave Starting Line",
@@ -113,9 +138,17 @@ var config_data = `
         "x": "Not Attempted"
       },
       "defaultValue": "x"
+    },
+    { "name": "Scored in<br>Opponent<br>Processor",
+      "code": "opp",
+      "type": "bool"
     }
   ],
   "endgame": [
+    { "name": "Barge Timer",
+      "code": "ebt",
+      "type": "timer"
+    },
     { "name": "Final Robot Status",
       "code": "efs",
       "type":"radio",
@@ -132,12 +165,12 @@ var config_data = `
   ],
   "postmatch": [
       {
-    "name": "Final Alliance Score",
+    "name": "Final match score",
     "code": "fms",
     "type": "number"   
     },
           {
-    "name": "Penalties",
+    "name": "fouls",
     "code": "penals",
     "type": "number"
     },
@@ -145,7 +178,13 @@ var config_data = `
       "code": "cop",
       "type": "bool"
     },
-
+    { "name": "Algae Left in Reef",
+      "code": "alr",
+      "type": "number",
+      "min": 0,
+      "max": 9,
+      "defaultValue": 0
+    },
     { "name": "Driver Skill",
       "code": "ds",
       "type": "radio",
